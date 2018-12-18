c7badec323cd7bcfe9be82c67ec0ab5182f90502
mcjcode <catalano_johnson@yahoo.com>
2018-12-17 20:24:08 -0500

    fixed map() usage that no longer works in py3

--- cholesky.py
+++ cholesky.py
@@ -29,9 +29,9 @@ def cholesky(mm):
 
 class CholeskyTest(unittest.TestCase):
     def runTest(self):
-        aa = map(lambda L: map(Fraction, L), [[5, 3, 3],
-                                              [3, 3, 3],
-                                              [3, 3, 8]])
+        aa = list(map(lambda L: list(map(Fraction, L)), [[5, 3, 3],
+                                                         [3, 3, 3],
+                                                         [3, 3, 8]]))
         m = np.array(aa)
         d, a = cholesky(m)
         atma = a.transpose().dot(m).dot(a)
