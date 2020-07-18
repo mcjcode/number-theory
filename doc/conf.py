8a4b41235d0be2bcc1188f86891607e2ec01660e
mcjcode <catalano_johnson@yahoo.com>
2020-07-18 16:16:16 -0400

    fixed pep8 violation

--- doc/conf.py
+++ doc/conf.py
@@ -27,9 +27,7 @@ author = 'mcjcode'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-'sphinx.ext.autodoc',
-'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
