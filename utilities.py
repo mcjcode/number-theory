8d6d28147c2f758f57f48aee9cd0dbbef6a11262
mcjcode <catalano_johnson@yahoo.com>
2022-12-14 19:44:35 -0500

    added digits and num_from_digits

--- utilities.py
+++ utilities.py
@@ -288,7 +288,7 @@ def order(a, p):
     """
     one = type(a)(1)
     cnt = 1
-    b = a
+    b = a % p
     while not b == one:
         b = (b * a) % p
         cnt += 1
@@ -515,6 +515,29 @@ def digitsum(n,base=10):
         retval += k
         n = (n-k)//base
     return retval
+
+
+def digits(n, base=10):
+    """
+    :param n: an integer
+    :return: a list ds so that the base^i digit of n is ds[i]
+    """
+    while n:
+        n, d = divmod(n, base)
+        yield d
+
+
+def num_from_digits(ds, base=10):
+    """
+    :param ds: a list of integers from [0,base)
+    :return: the integer whose base^i digit is ds[i]
+    """
+    n = 0
+    a = 1
+    for d in ds:
+        n += a*d
+        a *= base 
+    return n
 
 
 def sign(x):
