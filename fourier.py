a1b0a14b0967fcc7a5f7dbc9d1b531b8baea6812
mcjcode <catalano_johnson@yahoo.com>
2025-10-27 13:30:32 -0400

    added fast discrete fourier transform (dft)

--- fourier.py
+++ fourier.py
@@ -3,6 +3,9 @@
 #
 
 import numpy as np
+from utilities import primitive_root
+from factoring import factorize
+
 
 def fourierZ6():
     """
@@ -22,15 +25,16 @@ def fourierZ6():
 
     vi*vj = 0  (i!=j)
     vi*vi = vi
-    
+
     """
-    #                  v0   v1   v2   v3   v4   v5
-    return np.array([[  1,  -1,   0,  -1,   0,   1],   # g0
-                     [  0,   0,   0,   0, 1/2, 1/2],   # g1
-                     [  0, 1/2,-1/2,   0, 1/2,-1/2],   # g2
-                     [  0,   0,   0,   1,   0,  -1],   # g3
-                     [  0, 1/2, 1/2,   0,-1/2,-1/2],   # g4
-                     [  0,   0,   0,   0,-1/2, 1/2]])  # g5
+    #                v0   v1   v2   v3   v4   v5
+    return np.array([[1,  -1,   0,  -1,   0,   1],   # g0
+                     [0,   0,   0,   0, 1/2, 1/2],   # g1
+                     [0, 1/2,-1/2,   0, 1/2,-1/2],   # g2
+                     [0,   0,   0,   1,   0,  -1],   # g3
+                     [0, 1/2, 1/2,   0,-1/2,-1/2],   # g4
+                     [0,   0,   0,   0,-1/2, 1/2]])  # g5
+
 
 def fourierZ10():
     """
@@ -38,18 +42,19 @@ def fourierZ10():
     the regular representation of (Z/10Z,*)
     """
     vs = []
-    #                0   1   2    3   4   5   6   7   8    9
-    vs = np.array([[ 4,  0,  0 ,  0,  0,  0,  0,  0,  0 ,  0], # v0
-                   [-4,  0,  0 ,  0,  0,  4,  0,  0,  0 ,  0], # v1
-                   [-4,  0,  1 ,  0,  1,  0,  1,  0,  1 ,  0], # v2
-                   [ 0,  0, -1j,  0, -1,  0,  1,  0,  1j,  0], # v3
-                   [ 0,  0, -1 ,  0,  1,  0,  1,  0, -1 ,  0], # v4
-                   [ 0,  0,  1j,  0, -1,  0,  1,  0, -1j,  0], # v5
-                   [ 4,  1, -1 ,  1, -1, -4, -1,  1, -1 ,  1], # v6
-                   [ 0,  1,  1j, 1j,  1,  0, -1,-1j, -1j, -1], # v7
-                   [ 0,  1,  1 , -1, -1,  0, -1, -1,  1 ,  1], # v8
-                   [ 0,  1, -1j,-1j,  1,  0, -1, 1j,  1j, -1]]) # v9
+    #                0  1   2    3   4   5   6   7   8    9
+    vs = np.array([[+4, 0,  0 ,  0,  0,  0,  0,  0,  0,   0],   # v0
+                   [-4, 0,  0 ,  0,  0,  4,  0,  0,  0,   0],   # v1
+                   [-4, 0,  1 ,  0,  1,  0,  1,  0,  1,   0],   # v2
+                   [+0, 0, -1j,  0, -1,  0,  1,  0,  1j,  0],   # v3
+                   [+0, 0, -1 ,  0,  1,  0,  1,  0, -1,   0],   # v4
+                   [+0, 0,  1j,  0, -1,  0,  1,  0, -1j,  0],   # v5
+                   [+4, 1, -1 ,  1, -1, -4, -1,  1, -1,   1],   # v6
+                   [+0, 1,  1j, 1j,  1,  0, -1,-1j, -1j, -1],   # v7
+                   [+0, 1,  1 , -1, -1,  0, -1, -1,  1,   1],   # v8
+                   [+0, 1, -1j,-1j,  1,  0, -1, 1j,  1j, -1]])  # v9
     return np.array(vs/4).transpose()
+
 
 def generalized_hadamard_transform(a, m):
     """
@@ -57,15 +62,15 @@ def generalized_hadamard_transform(a, m):
     a is a 1d np.array with length a positive power of k
     """
     k = m.shape[0]
-    powkn = len(a) # 16
+    powkn = len(a)  # 16
     powk = 1
-    while powk < powkn: # 1, 2, 4, 8
+    while powk < powkn:  # 1, 2, 4, 8
         for j in range(0, powkn, k*powk):
             for i in range(0, powk):
                 a[j+i:j+i+k*powk:powk] = m.dot(a[j+i:j+i+k*powk:powk])
-            #a[j:j+k*powk:powk] = m.dot(a[j:j+k*powk:powk])
         powk *= k
-        
+
+
 def hadamard_transform(a):
     """
     Do an in-place 'fast' Walsh-Hadamard transform.
@@ -80,6 +85,7 @@ def hadamard_transform(a):
                 a[k1], a[k2] = a[k1]+a[k2], a[k1]-a[k2]
         pow2i *= 2
 
+
 def or_transform(a):
     pow2n = len(a)
     pow2i = 1
@@ -90,6 +96,7 @@ def or_transform(a):
                 a[k2] = a[k1]+a[k2]
         pow2i *= 2
 
+
 def inverse_or_transform(a):
     pow2n = len(a)
     pow2i = 1
@@ -99,4 +106,78 @@ def inverse_or_transform(a):
                 k2 = k1+pow2i
                 a[k2] = -a[k1]+a[k2]
         pow2i *= 2
-    
+
+
+def convolve(u, v, p):
+    n = u.shape[0]
+    w = np.zeros((n,), dtype=object)
+    for i in range(n):
+        for j in range(n):
+            w[i] = (w[i] + u[j]*v[(n+i-j)%n]) % p
+    return w
+
+
+def dft_reference(xs, p, inverse=False):
+    """
+    Just a reference implementation.  Correct but not a 'fft'
+    """
+    sgn = -1 if inverse else +1
+
+    n = len(xs)
+    if (p-1)%n:
+        raise ValueError(f'{n} does not divide the order of (Z/{p}Z)*')
+
+    a = primitive_root(p)
+    z = pow(a, (p-1)//n, p)
+
+    #
+    # z is now an nth root of unity (mod p)
+    #
+    ys = np.array([0]*n, dtype=object)
+
+    for i in range(n):
+        zsgni = pow(z, sgn*i, p)
+        coef = 1
+        for j in range(n):
+            ys[i] += coef*xs[j]
+            coef = (coef * zsgni) % p
+
+    return ys % p
+
+
+def _dft(xs, qs, p, a, inverse=False):
+    """
+    A safe version of the discrete fourier transform (where
+    we already know that the length of n divides p-1)
+    """
+    n = len(xs)
+    sgn = -1 if inverse else +1
+
+    if len(qs)==1:
+        return np.array([sum(pow(a, sgn*j*i, p)*xs[i] for i in range(n))
+                         for j in range(n)], dtype=object) % p
+
+    q = qs[0]
+    z = pow(a, q, p)
+
+    xs2 = np.zeros((n,), dtype=object)
+    for i in range(q):
+        xs2[i::q] = _dft(xs[i::q], qs[1:], p, z, inverse=inverse)
+
+    xs3 = np.zeros((n,), dtype=object)
+    for j in range(n):
+        sj = sgn*j
+        sj2 = j%(n//q)
+        xs3[j] = sum(pow(a, sj*v, p) * xs2[v+sj2*q] for v in range(q)) % p
+
+    return xs3
+
+
+def dft(xs, p, inverse=False):
+    n = len(xs)
+    if (p-1)%n:
+        raise ValueError(f'Length of xs ({n}) does not divide order of (Z/{p}Z)^*')
+    qs = sum(([q]*e for q, e in factorize(n)), [])
+    a = pow(primitive_root(p), (p-1)//n, p)
+
+    return _dft(xs, qs, p, a, inverse=inverse)
--- test/test_fourier.py
+++ test/test_fourier.py
@@ -11,7 +11,11 @@ from fourier import (
     inverse_or_transform,
     fourierZ6,
     fourierZ10,
+    dft_reference,
+    _dft,
+    dft,
 )
+
 
 class FourierTests(unittest.TestCase):
 
@@ -24,7 +28,7 @@ class FourierTests(unittest.TestCase):
                 ij = i*j % n
                 retval[ij] = (retval[ij] + v[i]*w[j])
         return retval
-    
+
     @staticmethod
     def check_orthogonality(m):
         m = m.transpose()
@@ -36,13 +40,14 @@ class FourierTests(unittest.TestCase):
                     if np.any(v!=w) or np.linalg.norm(np.array(v)-np.array(x)):
                         return False
         return True
-        
+
     def test_fourierZ6(self):
         self.assertTrue(FourierTests.check_orthogonality(fourierZ6()))
 
     def test_fourierZ10(self):
         self.assertTrue(FourierTests.check_orthogonality(fourierZ10()))
-        
+
+
 class GeneralizedHadamardTest(unittest.TestCase):
     """
     Make sure that the generalize_hadamard_transform
@@ -50,12 +55,12 @@ class GeneralizedHadamardTest(unittest.TestCase):
     hard-coded transforms
     """
     def test_transforms(self):
-        for transform, m in ((  hadamard_transform, [[ 1, 1],
-                                                     [ 1,-1]]),
-                             (        or_transform, [[ 1, 0],
-                                                     [ 1, 1]]),
-                             (inverse_or_transform, [[ 1, 0],
-                                                     [-1, 1]])):
+        for transform, m in ((hadamard_transform,   [[1,  1],
+                                                     [1, -1]]),
+                             (or_transform,         [[1,  0],
+                                                     [1,  1]]),
+                             (inverse_or_transform, [[1,  0],
+                                                    [-1,  1]])):
             m = np.array(m, dtype=np.int8)
             n = 2
             for _ in range(6):
@@ -71,3 +76,57 @@ class GeneralizedHadamardTest(unittest.TestCase):
                 self.assertTrue(np.all(vals[0]==vals[1].transpose()))
                 n *= 2
 
+
+def test_dft_reference_convolve():
+    def convolve(u, v, p):
+        n = u.shape[0]
+        w = np.zeros((n,), dtype=object)
+        for i in range(n):
+            for j in range(n):
+                w[i] = (w[i] + u[j]*v[(n+i-j)%n]) % p
+        return w
+
+    for p in [7, 13, 19, 31]:
+
+        p = 7
+        n = p-1
+        u = np.random.randint(0, p, n)
+        v = np.random.randint(0, p, n)
+
+        uv1 = convolve(u, v, p)
+
+        ut = dft_reference(u, p)
+        vt = dft_reference(v, p)
+
+        uv2 = (dft_reference(ut*vt, p, inverse=True)*pow(n, -1, p))%p
+
+        assert np.all(uv1==uv2)
+
+
+def test_inner_dft():
+    """
+    Test the _dft routine against the reference dft implementation
+    """
+
+    n = 30
+    p = 31
+    qs = [2, 3, 5]
+    a = 3  # a primitive root mod 30
+    for inverse in False, True:
+        for v1 in np.eye(n):
+            v2 = _dft(v1, qs=qs, p=p, a=a, inverse=inverse)
+            v3 = dft_reference(v1, p, inverse)
+            assert np.all(v2==v3)
+
+
+def test_dft():
+    """
+    Test dft against the reference implementation
+    """
+    n = 30
+    p = 31
+    for inverse in False, True:
+        for v in np.eye(n):
+            w1 = dft_reference(v, p, inverse=inverse)
+            w2 = dft(v, p, inverse=inverse)
+            assert np.all(w1==w2)
